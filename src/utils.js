43270ca7d044ac5116fa3672abfacbe4c55cff15
sezinzeydan <41441283+sezinzeydan@users.noreply.github.com>
2020-11-25 22:09:24 +0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -64,3 +64,47 @@ function quad(a, b, c, d, indices) {
     indices.push(c);
     indices.push(d);
 }
+
+//sphere
+function triangle(a, b, c, pointsArray, index) {
+     pointsArray.push(a);
+     pointsArray.push(b);      
+     pointsArray.push(c);
+     /* For shading
+     normalsArray.push(a[0],a[1], a[2], 0.0);
+     normalsArray.push(b[0],b[1], b[2], 0.0);
+     normalsArray.push(c[0],c[1], c[2], 0.0);
+	*/
+     index += 3;
+     
+}
+
+
+function divideTriangle(a, b, c, count) {
+    if ( count > 0 ) {
+                
+        var ab = mix( a, b, 0.5);
+        var ac = mix( a, c, 0.5);
+        var bc = mix( b, c, 0.5);
+                
+        ab = normalize(ab, true);
+        ac = normalize(ac, true);
+        bc = normalize(bc, true);
+                                
+        divideTriangle( a, ab, ac, count - 1 );
+        divideTriangle( ab, b, bc, count - 1 );
+        divideTriangle( bc, c, ac, count - 1 );
+        divideTriangle( ab, bc, ac, count - 1 );
+    }
+    else { 
+        triangle( a, b, c );
+    }
+}
+
+
+function tetrahedron(a, b, c, d, n) {
+    divideTriangle(a, b, c, n);
+    divideTriangle(d, c, b, n);
+    divideTriangle(a, d, b, n);
+    divideTriangle(a, c, d, n);
+}
