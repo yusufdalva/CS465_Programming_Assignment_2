feefea0ff7552a842279a887c5ccc33522c040e9
sezinzeydan <sezinzeydan51@gmail.com>
2020-11-28 11:02:11 +0300

    object shading

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ let program;
 let spherePointCount;
 let toSubdivide = 4;
 
+var radius = 1.5;
+var theta  = 0.0;
+var phi    = 0.0;
+var dr = 5.0 * Math.PI/180.0;
+
 let sphereMetadata = {};
 let cylinderMetadata = {};
 let prismMetadata = {};
@@ -16,8 +21,21 @@ let eye;
 let at = vec3(0.0, 0.0, 0.0);
 let up = vec3(0.0, 1.0, 0.0);
 
+//light stuff
+var lightPosition = vec4(1.0, 1.0, 1.0, 0 );
+var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
+var lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
+var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+
+var materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
+var materialDiffuse = vec4( 1.0, 0.8, 0.0, 1.0 );
+var materialSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+var ambientColor, diffuseColor, specularColor;
+var materialShininess = 50.0;
 let modelViewMatrix, projectionMatrix;
 let mvMatrixLoc, prjMatrixLoc;
+
+var normalMatrix, normalMatrixLoc;
 
 let instanceMatrix;
 
@@ -39,51 +57,94 @@ function init() {
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
-    program = initShaders(gl, "vertex-shader", "fragment-shader");
-    gl.useProgram(program);
+    
     gl.enable(gl.DEPTH_TEST);
 
-    // Generate Sphere Points
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    ambientProduct = mult(lightAmbient, materialAmbient);
+	console.log("AMBIENTT " +ambientProduct);
+    diffuseProduct = mult(lightDiffuse, materialDiffuse);
+	console.log(diffuseProduct);
+    specularProduct = mult(lightSpecular, materialSpecular);
+	console.log(specularProduct);
+  
+
+    
 
     // Generate Cylinder Points - uses index structure
     let cylinder = getPrism(30, 0, 0.2, 5, vertexCount);
     let cylinderVertices = cylinder[0];
     vertexCount += cylinderVertices.length;
     let cylinderIndices = cylinder[1];
+    let cylinderNormals = cylinder[2];
     cylinderMetadata.noOfSides = 30;
     cylinderMetadata.vertexCount = cylinderVertices.length;
     cylinderMetadata.vertices = cylinderVertices;
     cylinderMetadata.indexCount = cylinderIndices.length;
     cylinderMetadata.indices = cylinderIndices;
+    cylinderMetadata.normals = cylinderNormals;
+
+    console.log(cylinderMetadata.vertexCount);
+  
 
     // Generate Square Prism - uses index structure
     let prism = getPrism(4, 0, 0.5, 0.5, vertexCount);
     let prismVertices = prism[0];
     vertexCount += prismVertices.length;
     let prismIndices = prism[1];
+    let prismNormals = prism[2];
     prismMetadata.noOfSides = 4;
     prismMetadata.vertexCount = prismVertices.length;
     prismMetadata.vertices = prismVertices;
     prismMetadata.indexCount = prismIndices.length;
     prismMetadata.indices = prismIndices;
-
+    prismMetadata.normals = prismNormals;
+   
+      // Generate Sphere Points
     let sphere = tetrahedron(toSubdivide);
     let sphereVertices = sphere[1];
+    let sphereNormals = sphere[2];
     vertexCount += sphereVertices.length;
     spherePointCount = sphere[0];
     sphereMetadata.vertexCount = sphereVertices.length;
     sphereMetadata.vertices = sphereVertices;
+    sphereMetadata.normals = sphereNormals;
+    console.log("Sphere normals " + sphereNormals.length);
+    console.log("prism normals" + prismNormals.length);
+    console.log("cylinder normals " + cylinderNormals.length);
 
+    console.log("Sphere vertices " + sphereVertices.length);
+    console.log("prism vertices" + prismVertices.length);
+    console.log("cylinder vertices " + cylinderVertices.length);
+    
     let indices = cylinderIndices.concat(prismIndices);
 
     let vertices = cylinderVertices;
     vertices = vertices.concat(prismVertices);
     vertices = vertices.concat(sphereVertices);
-
+    console.log("total vERTICES" +vertices.length);
+   
+    let normals = cylinderMetadata.normals;
+    normals = normals.concat(prismMetadata.normals);
+    normals = normals.concat(sphereNormals);
+    console.log("total normals "+ normals.length);
+    
     console.log("Total number of vertices: " + vertices.length.toString());
     console.log("Total Number of indices: " + indices.length.toString());
 
     // All data transfer to the buffers are done here
+
+      //NORMALS
+    var nBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer);
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(normals), gl.STATIC_DRAW );
+      
+    var vNormal = gl.getAttribLocation( program, "vNormal" );
+    gl.vertexAttribPointer( vNormal, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vNormal);
+
     let iBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
@@ -95,19 +156,41 @@ function init() {
     let vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
+  
+
+    
+    
 
     mvMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
     prjMatrixLoc = gl.getUniformLocation(program, "projectionMatrix");
+    normalMatrixLoc = gl.getUniformLocation( program, "normalMatrix" );
 
     instanceMatrix = mat4(); // Initialize as identity matrix
     projectionMatrix = ortho(-10.0,10.0,-10.0, 10.0,-10.0,10.0);
     modelViewMatrix = mat4();
-
+    normalMatrix = mat3();
+    
     gl.uniformMatrix4fv(gl.getUniformLocation( program, "modelViewMatrix"), false, flatten(modelViewMatrix) );
     gl.uniformMatrix4fv( gl.getUniformLocation( program, "projectionMatrix"), false, flatten(projectionMatrix) );
+    gl.uniformMatrix3fv( gl.getUniformLocation( program, "normalMatrix"), false, flatten(normalMatrix));
+    document.getElementById("Button0").onclick = function(){radius *= 2.0;};
+    document.getElementById("Button1").onclick = function(){radius *= 0.5;};
+    document.getElementById("Button2").onclick = function(){theta += dr;};
+    document.getElementById("Button3").onclick = function(){theta -= dr;};
+    document.getElementById("Button4").onclick = function(){phi += dr;};
+    document.getElementById("Button5").onclick = function(){phi -= dr;};
 
     for(let i=0; i< numNodes; i++) initNodes(i);
-
+    gl.uniform4fv( gl.getUniformLocation(program, 
+        "ambientProduct"),flatten(ambientProduct) );
+     gl.uniform4fv( gl.getUniformLocation(program, 
+        "diffuseProduct"),flatten(diffuseProduct) );
+     gl.uniform4fv( gl.getUniformLocation(program, 
+        "specularProduct"),flatten(specularProduct) );	
+     gl.uniform4fv( gl.getUniformLocation(program, 
+        "lightPosition"),flatten(lightPosition) );
+     gl.uniform1f( gl.getUniformLocation(program, 
+        "shininess"),materialShininess );
     render();
 }
 
@@ -122,29 +205,28 @@ function render() {
 // Renders a sphere primitive
 function renderSphere() {
     for (let pointIdx = 0; pointIdx < sphereMetadata.vertexCount; pointIdx += 3) {
-        gl.drawArrays(gl.LINE_LOOP, pointIdx + cylinderMetadata.vertexCount + prismMetadata.vertexCount, 3); // Render in triangular wires
+        gl.drawArrays(gl.TRIANGLES, pointIdx + cylinderMetadata.vertexCount + prismMetadata.vertexCount, 3); // Render in triangular wires
     }
 }
 
 function renderCylinder() {
     let drawnPoints = 0;
-    gl.drawElements(gl.LINE_LOOP, cylinderMetadata.noOfSides, gl.UNSIGNED_BYTE, Uint8Array.BYTES_PER_ELEMENT * drawnPoints);
+    gl.drawElements(gl.TRIANGLE_FAN, cylinderMetadata.noOfSides, gl.UNSIGNED_BYTE, Uint8Array.BYTES_PER_ELEMENT * drawnPoints);
     drawnPoints += cylinderMetadata.noOfSides;
-    gl.drawElements(gl.LINE_LOOP, cylinderMetadata.noOfSides, gl.UNSIGNED_BYTE, Uint8Array.BYTES_PER_ELEMENT * drawnPoints);
+    gl.drawElements(gl.TRIANGLE_FAN, cylinderMetadata.noOfSides, gl.UNSIGNED_BYTE, Uint8Array.BYTES_PER_ELEMENT * drawnPoints);
     drawnPoints += cylinderMetadata.noOfSides;
-    gl.drawElements(gl.LINE_LOOP, cylinderMetadata.indexCount - drawnPoints, gl.UNSIGNED_BYTE, Uint8Array.BYTES_PER_ELEMENT * drawnPoints);
-
+    gl.drawElements(gl.TRIANGLE_FAN, cylinderMetadata.indexCount - drawnPoints, gl.UNSIGNED_BYTE, Uint8Array.BYTES_PER_ELEMENT * drawnPoints);
 }
 
 function renderPrism() {
     let drawnPoints = 0;
-    gl.drawElements(gl.LINE_LOOP, prismMetadata.noOfSides, gl.UNSIGNED_BYTE,
+    gl.drawElements(gl.TRIANGLE_FAN, prismMetadata.noOfSides, gl.UNSIGNED_BYTE,
         Uint8Array.BYTES_PER_ELEMENT * (drawnPoints + cylinderMetadata.indexCount));
     drawnPoints += prismMetadata.noOfSides;
-    gl.drawElements(gl.LINE_LOOP, prismMetadata.noOfSides, gl.UNSIGNED_BYTE,
+    gl.drawElements(gl.TRIANGLE_FAN, prismMetadata.noOfSides, gl.UNSIGNED_BYTE,
         Uint8Array.BYTES_PER_ELEMENT * (drawnPoints + cylinderMetadata.indexCount));
     drawnPoints += prismMetadata.noOfSides;
-    gl.drawElements(gl.LINE_LOOP, prismMetadata.indexCount - drawnPoints, gl.UNSIGNED_BYTE,
+    gl.drawElements(gl.TRIANGLE_FAN, prismMetadata.indexCount - drawnPoints, gl.UNSIGNED_BYTE,
         Uint8Array.BYTES_PER_ELEMENT * (drawnPoints + cylinderMetadata.indexCount));
 }
 function scale4(a, b, c) {
@@ -167,10 +249,11 @@ function createNode(transform, render, sibling, child){
 function traverse(Id) {
 
    if(Id == null) return;
-   console.log(Id);
-   console.log(spider[Id]);
    stack.push(modelViewMatrix);
    modelViewMatrix = mult(modelViewMatrix, spider[Id].transform);
+   eye = vec3(radius*Math.sin(theta)*Math.cos(phi), 
+        radius*Math.sin(theta)*Math.sin(phi), radius*Math.cos(theta));
+    modelViewMatrix = mult(modelViewMatrix,lookAt(eye,at,up));
    spider[Id].render();
    if(spider[Id].child != null) traverse(spider[Id].child);
     modelViewMatrix = stack.pop();
@@ -214,10 +297,10 @@ function initNodes(partId) {
 
         case leftMidLeg1Id:
             m = translate(2*upperLegHeight,0, 0.0);
-            m = mult(m,rotate(alpha[leftMidLeg1Id],1,0,0));
+            m = mult(m,rotate(alpha[leftMidLeg1Id],0,0,1));
             spider[leftMidLeg1Id] = createNode(m, midLeg,null,leftLowerLeg1Id);
             break;	
-		    
+	//BUNDA Bİ DEĞİŞİKLİK VAR
 	case leftLowerLeg1Id:
 	    m = translate(2*midLegHeight, 0.0, 0.0);
 	    m = mult(m,rotate(alpha[leftLowerLeg1Id],1,0,0));
@@ -232,13 +315,13 @@ function initNodes(partId) {
 
         case leftMidLeg2Id:
             m = translate(2*upperLegHeight,0, 0.0);
-            m = mult(m,rotate(alpha[leftMidLeg2Id],1,0,0));
+            m = mult(m,rotate(alpha[leftMidLeg2Id],0,0,1));
             spider[leftMidLeg2Id] = createNode(m, midLeg,null,leftLowerLeg2Id);
             break;
 		    
 	case leftLowerLeg2Id:
 	    m = translate(2*midLegHeight, 0.0, 0.0);
-	    m = mult(m,rotate(alpha[leftLowerLeg2Id],1,0,0));
+	    m = mult(m,rotate(alpha[leftLowerLeg2Id],0,0,1));
 	    spider[leftLowerLeg2Id] = createNode(m,lowerLeg,null,null);
 	    break;
 	
@@ -288,13 +371,13 @@ function initNodes(partId) {
 
         case rightMidLeg1Id:
             m = translate(2*upperLegHeight,0, 0.0);
-            m = mult(m,rotate(alpha[rightMidLeg1Id],1,0,0));
+            m = mult(m,rotate(alpha[rightMidLeg1Id],0,0,1));
             spider[rightMidLeg1Id] = createNode(m, midLeg,null,rightLowerLeg1Id);
             break;
 			
 	case rightLowerLeg1Id:
 	    m = translate(2*midLegHeight, 0.0, 0.0);
-	    m = mult(m,rotate(alpha[rightLowerLeg1Id],1,0,0));
+	    m = mult(m,rotate(alpha[rightLowerLeg1Id],0,0,1));
 	    spider[rightLowerLeg1Id] = createNode(m,lowerLeg,null,null);
 	    break;
 			
@@ -307,13 +390,13 @@ function initNodes(partId) {
 
         case rightMidLeg2Id:
             m = translate(2*upperLegHeight,0.0, 0.0);
-            m = mult(m,rotate(alpha[rightMidLeg2Id],1,0,0));
+            m = mult(m,rotate(alpha[rightMidLeg2Id],0,0,1));
             spider[rightMidLeg2Id] = createNode(m, midLeg,null,rightLowerLeg2Id);
             break;
 			
 	case rightLowerLeg2Id:
 	    m = translate(2*midLegHeight, 0.0, 0.0);
-	    m = mult(m,rotate(alpha[rightLowerLeg2Id],1,0,0));
+	    m = mult(m,rotate(alpha[rightLowerLeg2Id],0,0,1));
 	    spider[rightLowerLeg2Id] = createNode(m,lowerLeg,null,null);
 	    break;
 	//right leg 3
@@ -325,13 +408,13 @@ function initNodes(partId) {
 
         case rightMidLeg3Id:
             m = translate(2*upperLegHeight,0.0, 0.0);
-            m = mult(m,rotate(alpha[rightMidLeg3Id],1,0,0));
+            m = mult(m,rotate(alpha[rightMidLeg3Id],0,0,1));
             spider[rightMidLeg3Id] = createNode(m, midLeg,null,rightLowerLeg3Id);
             break;
 			
 	case rightLowerLeg3Id:
 	    m = translate(2*midLegHeight, 0.0, 0.0);
-	    m = mult(m,rotate(alpha[rightLowerLeg3Id],1,0,0));
+	    m = mult(m,rotate(alpha[rightLowerLeg3Id],0,0,1));
 	    spider[rightLowerLeg3Id] = createNode(m,lowerLeg,null,null);
 	    break;
 	//right leg 4
@@ -343,13 +426,13 @@ function initNodes(partId) {
 
         case rightMidLeg4Id:
             m = translate(2*upperLegHeight,0.0, 0.0);
-            m = mult(m,rotate(alpha[rightMidLeg4Id],1,0,0));
+            m = mult(m,rotate(alpha[rightMidLeg4Id],0,0,1));
             spider[rightMidLeg4Id] = createNode(m, midLeg,null,rightLowerLeg4Id);
             break;
 			
 	case rightLowerLeg4Id:
 	    m = translate(2*midLegHeight, 0.0, 0.0);
-	    m = mult(m,rotate(alpha[rightLowerLeg4Id],1,0,0));
+	    m = mult(m,rotate(alpha[rightLowerLeg4Id],0,0,1));
 	    spider[rightLowerLeg4Id] = createNode(m,lowerLeg,null,null);
 	    break;
     }
@@ -360,8 +443,19 @@ function initNodes(partId) {
 // Render Functions for parts
 // Applies post-multiplication
 function torso() {
+    
     instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.0, 0.0));
     instanceMatrix = mult(instanceMatrix, scale4(torsoWidth, torsoHeight, torsoWidth));
+    
+   
+    normalMatrix = [
+        vec3(modelViewMatrix[0][0], modelViewMatrix[0][1], modelViewMatrix[0][2]),
+        vec3(modelViewMatrix[1][0], modelViewMatrix[1][1], modelViewMatrix[1][2]),
+        vec3(modelViewMatrix[2][0], modelViewMatrix[2][1], modelViewMatrix[2][2])
+    ];
+    console.log(normalMatrix);
+    //normalMatrix = transpose(normalMatrix);
+    gl.uniformMatrix3fv(normalMatrixLoc, false, flatten(normalMatrix) );
     gl.uniformMatrix4fv(mvMatrixLoc, false, flatten(instanceMatrix));
     renderSphere();
 }
@@ -369,6 +463,12 @@ function torso() {
 function tentacle() {
     instanceMatrix = mult(modelViewMatrix, translate(0.0, tentacleHeight * 0.35, 0));
     instanceMatrix = mult(instanceMatrix, scale4(tentacleMid, tentacleHeight, tentacleMid));
+    normalMatrix = [
+        vec3(modelViewMatrix[0][0], modelViewMatrix[0][1], modelViewMatrix[0][2]),
+        vec3(modelViewMatrix[1][0], modelViewMatrix[1][1], modelViewMatrix[1][2]),
+        vec3(modelViewMatrix[2][0], modelViewMatrix[2][1], modelViewMatrix[2][2])
+    ];
+    gl.uniformMatrix3fv(normalMatrixLoc, false, flatten(normalMatrix) );
     gl.uniformMatrix4fv(mvMatrixLoc, false, flatten(instanceMatrix));
     renderPrism();
 }
@@ -376,6 +476,12 @@ function tentacle() {
 function lowerTorso() {
     instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.0, 0.0));
     instanceMatrix = mult(instanceMatrix, scale4(lowerTorsoWidth, lowerTorsoHeight, lowerTorsoWidth));
+   normalMatrix = [
+        vec3(modelViewMatrix[0][0], modelViewMatrix[0][1], modelViewMatrix[0][2]),
+        vec3(modelViewMatrix[1][0], modelViewMatrix[1][1], modelViewMatrix[1][2]),
+        vec3(modelViewMatrix[2][0], modelViewMatrix[2][1], modelViewMatrix[2][2])
+    ];
+    gl.uniformMatrix3fv(normalMatrixLoc, false, flatten(normalMatrix) );
     gl.uniformMatrix4fv(mvMatrixLoc, false, flatten(instanceMatrix));
     renderSphere();
 }
@@ -383,6 +489,12 @@ function lowerTorso() {
 function upperLeg() {
     instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.0, 0.0));
     instanceMatrix = mult(instanceMatrix, scale4(upperLegHeight, upperLegWidth, upperLegHeight));
+    normalMatrix = [
+        vec3(modelViewMatrix[0][0], modelViewMatrix[0][1], modelViewMatrix[0][2]),
+        vec3(modelViewMatrix[1][0], modelViewMatrix[1][1], modelViewMatrix[1][2]),
+        vec3(modelViewMatrix[2][0], modelViewMatrix[2][1], modelViewMatrix[2][2])
+    ];
+    gl.uniformMatrix3fv(normalMatrixLoc, false, flatten(normalMatrix) );
     gl.uniformMatrix4fv(mvMatrixLoc, false, flatten(instanceMatrix));
     renderCylinder();
 }
@@ -390,6 +502,12 @@ function upperLeg() {
 function midLeg() {
     instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.0, 0.0));
     instanceMatrix = mult(instanceMatrix, scale4(midLegHeight, midLegWidth, midLegHeight));
+   normalMatrix = [
+        vec3(modelViewMatrix[0][0], modelViewMatrix[0][1], modelViewMatrix[0][2]),
+        vec3(modelViewMatrix[1][0], modelViewMatrix[1][1], modelViewMatrix[1][2]),
+        vec3(modelViewMatrix[2][0], modelViewMatrix[2][1], modelViewMatrix[2][2])
+    ];
+    gl.uniformMatrix3fv(normalMatrixLoc, false, flatten(normalMatrix) );
     gl.uniformMatrix4fv(mvMatrixLoc, false, flatten(instanceMatrix));
     renderCylinder();
 }
@@ -397,6 +515,12 @@ function midLeg() {
 function lowerLeg(){
     instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.0, 0.0));
     instanceMatrix = mult(instanceMatrix, scale4(lowerLegHeight, lowerLegWidth, lowerLegHeight));
+    normalMatrix = [
+        vec3(modelViewMatrix[0][0], modelViewMatrix[0][1], modelViewMatrix[0][2]),
+        vec3(modelViewMatrix[1][0], modelViewMatrix[1][1], modelViewMatrix[1][2]),
+        vec3(modelViewMatrix[2][0], modelViewMatrix[2][1], modelViewMatrix[2][2])
+    ];
+    gl.uniformMatrix3fv(normalMatrixLoc, false, flatten(normalMatrix) );
     gl.uniformMatrix4fv(mvMatrixLoc, false, flatten(instanceMatrix));
     renderCylinder();
 }
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,9 @@
 let index = 0;
 let pointsArray = [];
 let indices = [];
-
+let normalsArray = [];
+let vertices = [];//vertices of prism
+let normals = [];//normals of prism
 function matrixVectorProd(matrix, vector) {
     if (matrix.length !== vector.length) {
         return null;
@@ -38,6 +40,7 @@ function getPolygon(sides, rotateAxis, r) {
     let vertices = [startVertex];
     for (let rotationCount = 0; vertices.length < sides; rotationCount++) {
         startVertex = matrixVectorProd(rotateMatrix, startVertex);
+        //startVertex = normalize(startVertex,true);
         vertices.push(startVertex);
     }
     return vertices;
@@ -45,24 +48,42 @@ function getPolygon(sides, rotateAxis, r) {
 
 function getPrism(sides, rotateAxis, radius, height, startIndex) {
     indices = [];
-    let circleVertices = getPolygon(sides, 2, radius);
+    normals = [];
+    let circleVertices = getPolygon(sides, rotateAxis, radius);
+    vertices = [];
     for (let i = 0; i < 2 * sides; i++) { indices.push(i + startIndex); }
     const cylinderHeight = Math.min(height, 2);
     let bottom = [];
     let top = [];
+    
+    
     for (let vertexIdx = 0; vertexIdx < circleVertices.length; vertexIdx++) {
         if (rotateAxis == 2) {
-            bottom.push(matrixVectorProd(translate(0, 0, 0), circleVertices[vertexIdx]));
-            top.push(matrixVectorProd(translate(0, 0, cylinderHeight), circleVertices[vertexIdx]));
+            let down = matrixVectorProd(translate(0, 0, 0), circleVertices[vertexIdx]);
+            let up = matrixVectorProd(translate(0, 0, cylinderHeight), circleVertices[vertexIdx]);
+
+            bottom.push(down);
+            top.push(up);
+            
         } else if (rotateAxis == 1) {
-            bottom.push(matrixVectorProd(translate(0, 0, 0), circleVertices[vertexIdx]));
-            top.push(matrixVectorProd(translate(0, cylinderHeight, 0), circleVertices[vertexIdx]));
+            let down = matrixVectorProd(translate(0, 0, 0), circleVertices[vertexIdx]); 
+            let up = matrixVectorProd(translate(0, cylinderHeight, 0), circleVertices[vertexIdx]);
+            
+            bottom.push(down);
+            top.push(up);
+           
         } else if (rotateAxis == 0) {
-            bottom.push(matrixVectorProd(translate(0, 0, 0), circleVertices[vertexIdx]));
-            top.push(matrixVectorProd(translate(cylinderHeight, 0, 0), circleVertices[vertexIdx]));
+            let down = matrixVectorProd(translate(0, 0, 0), circleVertices[vertexIdx]);
+            let up = matrixVectorProd(translate(cylinderHeight, 0, 0), circleVertices[vertexIdx]);
+        
+            bottom.push(down);
+            top.push(up);
+            
         }
     }
-    let vertices = bottom.concat(top);
+    
+   generateNormals(bottom,top,sides);
+    
     for (let side = 0; side < sides; side++) {
         let bottomIndices = [(side % sides), ((side + 1) % sides)];
         let topIndices = [(side % sides) + sides, ((side + 1) % sides) + sides];
@@ -71,20 +92,72 @@ function getPrism(sides, rotateAxis, radius, height, startIndex) {
         indices.push(bottomIndices[0] + startIndex);
         indices.push(bottomIndices[1] + startIndex);
         indices.push(topIndices[1] + startIndex);
+     
     }
-    return [vertices, indices];
+    
+    return [vertices, indices,normals];
 }
+
+//generate normals
+function generateNormals(bottom,top,sides){
+    //find bottom normals
+    
+    let t1 = subtract(bottom[1], bottom[0]);
+    let t2 = subtract(bottom[2], bottom[1]);
+    let normal = cross(t1, t2);
+    bottomNormal = vec3(normal);
+    for(let i = 0; i < sides;i++){
+        vertices.push(bottom[i]);
+        normals.push(bottomNormal);
+    }
+    
+    //setting side normals and vertices
+    startVertex = 0;
+    for(let j = 0; j < sides; j++){
+        
+        quad(top[j],top[(j+1)%sides],bottom[j],bottom[(j+1)%sides]);
+    }
+    //top normals
+    for(let k = 0; k < sides;k++){
+        vertices.push(top[k]);
+        normals.push(-bottomNormal);
+    }
+}
+//generating vertices and normals for sides of prism
+function quad(a, b, c, d) {
+    console.log(a);
+    let t1 = subtract(b, d);
+    let t2 = subtract(a, d);
+    let normal = cross(t1, t2);
+    normal = vec3(normal);
+
+
+    vertices.push(a); 
+    normals.push(normal); 
+    vertices.push(b); 
+    normals.push(normal); 
+    vertices.push(c); 
+    normals.push(normal); 
+    vertices.push(a); 
+    normals.push(normal);
+    vertices.push(c); 
+    normals.push(normal);
+    vertices.push(d); 
+    normals.push(normal);
+
+}
+
 
 //sphere
 function triangle(a, b, c) {
      pointsArray.push(a);
      pointsArray.push(b);      
      pointsArray.push(c);
-     /* For shading
+    
      normalsArray.push(a[0],a[1], a[2], 0.0);
      normalsArray.push(b[0],b[1], b[2], 0.0);
      normalsArray.push(c[0],c[1], c[2], 0.0);
-	*/
+	
      index += 3;
 }
 
@@ -114,6 +187,7 @@ function divideTriangle(a, b, c, count) {
 function tetrahedron(n) {
     index = 0;
     pointsArray = [];
+    normalsArray = [];
     let a = vec4(0.0, 0.0, -1.0, 1);
     let b = vec4(0.0, 0.942809, 0.333333, 1);
     let c = vec4(-0.816497, -0.471405, 0.333333, 1);
@@ -122,5 +196,5 @@ function tetrahedron(n) {
     divideTriangle(d, c, b, n);
     divideTriangle(a, d, b, n);
     divideTriangle(a, c, d, n);
-    return [index, pointsArray];
+    return [index, pointsArray,normalsArray];
 }
