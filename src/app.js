5cd4071d77e1f9d09d86db22fbba191268c364cb
sezinzeydan <sezinzeydan51@gmail.com>
2020-11-29 12:16:28 +0300

    joint angles and sliders

--- src/app.js
+++ src/app.js
@@ -16,34 +16,34 @@ let prismMetadata = {};
 let vertexCount = 0;
 
 let alpha = [ // alpha is the joint angle
-    [0, 0, 0],      // torso
+    [0, 30, 115],      // torso
     [0, 30, 0],     // leftTentacle
     [0, 0, 0],      // rightTentacle
     [0, 0, 0],      // lowerTorso
-    [0, 0, 135],    // leftUpperLeg1
-    [0, 0, 0],      // leftMidLeg1
-    [0, 0, 0],      // leftLowerLeg1
-    [0, 0, 135],    // leftUpperLeg2
-    [0, 0, 0],      // leftMidLeg2Id
-    [0, 0, 0],      // leftLowerLeg2
-    [0, 0, 225],    // leftUpperLeg3
-    [0, 0, 0],      // leftMidLeg3
-    [0, 0, 0],      // leftLowerLeg3
-    [0, 0, 225],    // leftUpperLeg4
-    [0, 0, 0],      // leftMidLeg4
-    [0, 0, 0],      // leftLowerLeg4
-    [0, 0, 45],     // rightUpperLeg1
-    [0, 0, 0],      // rightMidLeg1
-    [0, 0, 0],      // rightLowerLeg1
-    [0, 0, 45],     // rightUpperLeg2
-    [0, 0, 0],      // rightMidLeg2
-    [0, 0, 0],      // rightLowerLeg2
-    [0, 0, 315],    // rightUpperLeg3
-    [0, 0, 0],      // rightMidLeg3
-    [0, 0, 0],      // rightLowerLeg3
-    [0, 0, 315],    // rightUpperLeg4
-    [0, 0, 0],      // rightMidLeg4
-    [0, 0, 0]       // rightLowerLeg4
+    [0, 35, 135],    // leftUpperLeg1
+    [0, 25, 0],      // leftMidLeg1
+    [0, 30, 0],      // leftLowerLeg1
+    [0, 35, 135],    // leftUpperLeg2
+    [0, 25, 0],      // leftMidLeg2Id
+    [0, 30, 0],      // leftLowerLeg2
+    [0, 35, 200],    // leftUpperLeg3
+    [0, 25, 0],      // leftMidLeg3
+    [0, 30, 0],      // leftLowerLeg3
+    [0, 35, 225],    // leftUpperLeg4
+    [0, 25, 0],      // leftMidLeg4
+    [0, 30, 0],      // leftLowerLeg4
+    [0, 35, 35],     // rightUpperLeg1
+    [0, 25, 0],      // rightMidLeg1
+    [0, 30, 0],      // rightLowerLeg1
+    [0, 35, 35],     // rightUpperLeg2
+    [0, 25, 0],      // rightMidLeg2
+    [0, 30, 0],      // rightLowerLeg2
+    [0, 35, 315],    // rightUpperLeg3
+    [0, 25, 0],      // rightMidLeg3
+    [0, 30, 0],      // rightLowerLeg3
+    [0, 35, 315],    // rightUpperLeg4
+    [0, 25, 0],      // rightMidLeg4
+    [0, 30, 0]       // rightLowerLeg4
 ];
 
 let eye;
@@ -174,7 +174,8 @@ function init() {
     normalMatrixLoc = gl.getUniformLocation( program, "normalMatrix" );
 
     instanceMatrix = mat4(); // Initialize as identity matrix
-    projectionMatrix = ortho(-10.0,10.0,-10.0, 10.0,-10.0,10.0);
+    projectionMatrix = ortho(-canvas.width/40, canvas.width/40, -canvas.height/40, canvas.height/40,-25.0,25.0);
+	
     modelViewMatrix = mat4();
     normalMatrix = mat3();
     
@@ -185,54 +186,6 @@ function init() {
     document.getElementById("Button3").onclick = function(){theta -= dr;};
     document.getElementById("Button4").onclick = function(){phi += dr;};
     document.getElementById("Button5").onclick = function(){phi -= dr;};
-
-    //SLIDERSSS********************
-    /*document.getElementById("slider0").onchange = function() {
-        theta[torsoId ] = event.srcElement.value;
-        initNodes(torsoId);
-    };
-        document.getElementById("slider1").onchange = function() {
-        theta[head1Id] = event.srcElement.value;
-        initNodes(head1Id);
-    };
-
-    document.getElementById("slider2").onchange = function() {
-         theta[leftUpperArmId] = event.srcElement.value;
-         initNodes(leftUpperArmId);
-    };
-    document.getElementById("slider3").onchange = function() {
-         theta[leftLowerArmId] =  event.srcElement.value;
-         initNodes(leftLowerArmId);
-    };
-     
-        document.getElementById("slider4").onchange = function() {
-        theta[rightUpperArmId] = event.srcElement.value;
-        initNodes(rightUpperArmId);
-    };
-    document.getElementById("slider5").onchange = function() {
-         theta[rightLowerArmId] =  event.srcElement.value;
-         initNodes(rightLowerArmId);
-    };
-        document.getElementById("slider6").onchange = function() {
-        theta[leftUpperLegId] = event.srcElement.value;
-        initNodes(leftUpperLegId);
-    };
-    document.getElementById("slider7").onchange = function() {
-         theta[leftLowerLegId] = event.srcElement.value;
-         initNodes(leftLowerLegId);
-    };
-    document.getElementById("slider8").onchange = function() {
-         theta[rightUpperLegId] =  event.srcElement.value;
-         initNodes(rightUpperLegId);
-    };
-        document.getElementById("slider9").onchange = function() {
-        theta[rightLowerLegId] = event.srcElement.value;
-        initNodes(rightLowerLegId);
-    };
-    document.getElementById("slider10").onchange = function() {
-         theta[head2Id] = event.srcElement.value;
-         initNodes(head2Id);
-    };*/
 
     for(let i=0; i< numNodes; i++) initNodes(i);
     gl.uniform4fv( gl.getUniformLocation(program, 
@@ -253,7 +206,6 @@ window.onload = init;
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     traverse(torsoId);
-    //renderPrism();
     requestAnimFrame(render);
 }
 
@@ -330,11 +282,9 @@ function initNodes(partId) {
             break;
         case leftTentacleId: // y - DONE
             m = translate(-(torsoWidth/2), torsoHeight,0.0);
-          //  m = mult(m, rotate(alpha[leftTentacleId], 0, 1, 0));
             m = mult(m, rotate(alpha[leftTentacleId][2], 0, 0, 1));
             m = mult(m, rotate(alpha[leftTentacleId][1], 0, 1, 0));
             m = mult(m, rotate(alpha[leftTentacleId][0], 1, 0, 0));
-            //m = mult(m,rotate(180,0,1,0));
             spider[leftTentacleId] = createNode(m, tentacle, rightTentacleId, null);
             break;
 
@@ -343,7 +293,6 @@ function initNodes(partId) {
             m = mult(m, rotate(alpha[rightTentacleId][2], 0, 0, 1));
             m = mult(m, rotate(alpha[rightTentacleId][1], 0, 1, 0));
             m = mult(m, rotate(alpha[rightTentacleId][0], 1, 0, 0));
-            // m = mult(m,rotate(180,0,1,0));
             spider[rightTentacleId] = createNode(m, tentacle, lowerTorsoId, null);
             break;
 
@@ -633,4 +582,416 @@ function lowerLeg(){
     renderCylinder();
 }
 
+//torso movements
+function moveHeadX(){
+   alpha[torsoId][0]= event.srcElement.value;
+   initNodes(torsoId);
+}
 
+function moveHeadY(){
+   alpha[torsoId][1]= event.srcElement.value;
+   initNodes(torsoId);
+}
+function moveHeadZ(){
+   alpha[torsoId][2]= event.srcElement.value;
+   initNodes(torsoId);
+}
+//Left tentacle
+function moveLTentacleX(){
+    alpha[leftTentacleId][0]= event.srcElement.value;
+   initNodes(leftTentacleId);
+}
+
+function moveLTentacleY(){
+    alpha[leftTentacleId][1]= event.srcElement.value;
+   initNodes(leftTentacleId);
+}
+function moveLTentacleZ(){
+    alpha[rightTentacleId][2]= event.srcElement.value;
+   initNodes(rightTentacleId);
+}
+
+//Right Tentacle
+function moveRTentacleX(){
+    alpha[rightTentacleId][0]= event.srcElement.value;
+   initNodes(rightTentacleId);
+}
+
+function moveRTentacleY(){
+    alpha[rightTentacleId][1]= event.srcElement.value;
+   initNodes(rightTentacleId);
+}
+function moveRTentacleZ(){
+    alpha[rightTentacleId][2]= event.srcElement.value;
+   initNodes(rightTentacleId);
+}
+
+
+//FIRST LEFT LEG
+function moveLeftUpper1X(){
+    alpha[leftUpperLeg1Id][0]= event.srcElement.value;
+    initNodes(leftUpperLeg1Id);
+}
+
+function moveLeftUpper1Y(){
+    alpha[leftUpperLeg1Id][1]= event.srcElement.value;
+    initNodes(leftUpperLeg1Id);
+}
+
+function moveLeftUpper1Z(){
+    alpha[leftUpperLeg1Id][2]= event.srcElement.value;
+    initNodes(leftUpperLeg1Id);
+}
+
+function moveLeftMid1X(){
+    alpha[leftMidLeg1Id][0]= event.srcElement.value;
+    initNodes(leftMidLeg1Id);
+}
+
+function moveLeftMid1Y(){
+    alpha[leftMidLeg1Id][1]= event.srcElement.value;
+    initNodes(leftMidLeg1Id);
+}
+
+function moveLeftMid1Z(){
+    alpha[leftMidLeg1Id][2]= event.srcElement.value;
+    initNodes(leftMidLeg1Id);
+}
+
+function moveLeftLower1X(){
+    alpha[leftLowerLeg1Id][0]= event.srcElement.value;
+    initNodes(leftLowerLeg1Id);
+}
+
+function moveLeftLower1Y(){
+    alpha[leftLowerLeg1Id][1]= event.srcElement.value;
+    initNodes(leftLowerLeg1Id);
+}
+
+function moveLeftLower1Z(){
+    alpha[leftLowerLeg1Id][2]= event.srcElement.value;
+    initNodes(leftLowerLeg1Id);
+}
+
+//SECOND LEFT LEG
+function moveLeftUpper2X(){
+    alpha[leftUpperLeg2Id][0]= event.srcElement.value;
+    initNodes(leftUpperLeg2Id);
+}
+
+function moveLeftUpper2Y(){
+    alpha[leftUpperLeg2Id][1]= event.srcElement.value;
+    initNodes(leftUpperLeg2Id);
+}
+
+function moveLeftUpper2Z(){
+    alpha[leftUpperLeg2Id][2]= event.srcElement.value;
+    initNodes(leftUpperLeg2Id);
+}
+
+function moveLeftMid2X(){
+    alpha[leftMidLeg2Id][0]= event.srcElement.value;
+    initNodes(leftMidLeg2Id);
+}
+
+function moveLeftMid2Y(){
+    alpha[leftMidLeg2Id][1]= event.srcElement.value;
+    initNodes(leftMidLeg2Id);
+}
+
+function moveLeftMid2Z(){
+    alpha[leftMidLeg2Id][2]= event.srcElement.value;
+    initNodes(leftMidLeg2Id);
+}
+
+function moveLeftLower2X(){
+    alpha[leftLowerLeg2Id][0]= event.srcElement.value;
+    initNodes(leftLowerLeg2Id);
+}
+
+function moveLeftLower2Y(){
+    alpha[leftLowerLeg2Id][1]= event.srcElement.value;
+    initNodes(leftLowerLeg2Id);
+}
+
+function moveLeftLower2Z(){
+    alpha[leftLowerLeg2Id][2]= event.srcElement.value;
+    initNodes(leftLowerLeg2Id);
+}
+
+//THIRD LEFT LEG
+function moveLeftUpper3X(){
+    alpha[leftUpperLeg3Id][0]= event.srcElement.value;
+    initNodes(leftUpperLeg3Id);
+}
+
+function moveLeftUpper3Y(){
+    alpha[leftUpperLeg3Id][1]= event.srcElement.value;
+    initNodes(leftUpperLeg3Id);
+}
+
+function moveLeftUpper3Z(){
+    alpha[leftUpperLeg3Id][2]= event.srcElement.value;
+    initNodes(leftUpperLeg3Id);
+}
+
+function moveLeftMid3X(){
+    alpha[leftMidLeg3Id][0]= event.srcElement.value;
+    initNodes(leftMidLeg3Id);
+}
+
+function moveLeftMid3Y(){
+    alpha[leftMidLeg3Id][1]= event.srcElement.value;
+    initNodes(leftMidLeg3Id);
+}
+
+function moveLeftMid3Z(){
+    alpha[leftMidLeg3Id][2]= event.srcElement.value;
+    initNodes(leftMidLeg3Id);
+}
+
+function moveLeftLower3X(){
+    alpha[leftLowerLeg3Id][0]= event.srcElement.value;
+    initNodes(leftLowerLeg3Id);
+}
+
+function moveLeftLower3Y(){
+    alpha[leftLowerLeg3Id][1]= event.srcElement.value;
+    initNodes(leftLowerLeg3Id);
+}
+
+function moveLeftLower3Z(){
+    alpha[leftLowerLeg3Id][2]= event.srcElement.value;
+    initNodes(leftLowerLeg3Id);
+}
+
+//FOURTH LEFT LEG
+function moveLeftUpper4X(){
+    alpha[leftUpperLeg4Id][0]= event.srcElement.value;
+    initNodes(leftUpperLeg4Id);
+}
+
+function moveLeftUpper4Y(){
+    alpha[leftUpperLeg4Id][1]= event.srcElement.value;
+    initNodes(leftUpperLeg4Id);
+}
+
+function moveLeftUpper4Z(){
+    alpha[leftUpperLeg4Id][2]= event.srcElement.value;
+    initNodes(leftUpperLeg4Id);
+}
+
+function moveLeftMid4X(){
+    alpha[leftMidLeg4Id][0]= event.srcElement.value;
+    initNodes(leftMidLeg4Id);
+}
+
+function moveLeftMid4Y(){
+    alpha[leftMidLeg4Id][1]= event.srcElement.value;
+    initNodes(leftMidLeg4Id);
+}
+
+function moveLeftMid4Z(){
+    alpha[leftMidLeg4Id][2]= event.srcElement.value;
+    initNodes(leftMidLeg4Id);
+}
+
+function moveLeftLower4X(){
+    alpha[leftLowerLeg4Id][0]= event.srcElement.value;
+    initNodes(leftLowerLeg4Id);
+}
+
+function moveLeftLower4Y(){
+    alpha[leftLowerLeg4Id][1]= event.srcElement.value;
+    initNodes(leftLowerLeg4Id);
+}
+
+function moveLeftLower4Z(){
+    alpha[leftLowerLeg4Id][2]= event.srcElement.value;
+    initNodes(leftLowerLeg4Id);
+}
+
+//RIGHT SIDE******
+//FIRST RIGHT LEG 
+function moveRightUpper1X(){
+    alpha[rightUpperLeg1Id][0]= event.srcElement.value;
+    initNodes(rightUpperLeg1Id);
+}
+
+function moveRightUpper1Y(){
+    alpha[rightUpperLeg1Id][1]= event.srcElement.value;
+    initNodes(rightUpperLeg1Id);
+}
+
+function moveRightUpper1Z(){
+    alpha[rightUpperLeg1Id][2]= event.srcElement.value;
+    initNodes(rightUpperLeg1Id);
+}
+
+function moveRightMid1X(){
+    alpha[rightMidLeg1Id][0]= event.srcElement.value;
+    initNodes(rightMidLeg1Id);
+}
+
+function moveRightMid1Y(){
+    alpha[rightMidLeg1Id][1]= event.srcElement.value;
+    initNodes(rightMidLeg1Id);
+}
+
+function moveRightMid1Z(){
+    alpha[rightMidLeg1Id][2]= event.srcElement.value;
+    initNodes(rightMidLeg1Id);
+}
+
+function moveRightLower1X(){
+    alpha[rightLowerLeg1Id][0]= event.srcElement.value;
+    initNodes(rightLowerLeg1Id);
+}
+
+function moveRightLower1Y(){
+    alpha[rightLowerLeg1Id][1]= event.srcElement.value;
+    initNodes(rightLowerLeg1Id);
+}
+
+function moveRightLower1Z(){
+    alpha[rightLowerLeg1Id][2]= event.srcElement.value;
+    initNodes(rightLowerLeg1Id);
+}
+
+//SECOND right LEG
+function moveRightUpper2X(){
+    alpha[rightUpperLeg2Id][0]= event.srcElement.value;
+    initNodes(rightUpperLeg2Id);
+}
+
+function moveRightUpper2Y(){
+    alpha[rightUpperLeg2Id][1]= event.srcElement.value;
+    initNodes(rightUpperLeg2Id);
+}
+
+function moveRightUpper2Z(){
+    alpha[rightUpperLeg2Id][2]= event.srcElement.value;
+    initNodes(rightUpperLeg2Id);
+}
+
+function moveRightMid2X(){
+    alpha[rightMidLeg2Id][0]= event.srcElement.value;
+    initNodes(rightMidLeg2Id);
+}
+
+function moveRightMid2Y(){
+    alpha[rightMidLeg2Id][1]= event.srcElement.value;
+    initNodes(rightMidLeg2Id);
+}
+
+function moveRightMid2Z(){
+    alpha[rightMidLeg2Id][2]= event.srcElement.value;
+    initNodes(rightMidLeg2Id);
+}
+
+function moveRightLower2X(){
+    alpha[rightLowerLeg2Id][0]= event.srcElement.value;
+    initNodes(rightLowerLeg2Id);
+}
+
+function moveRightLower2Y(){
+    alpha[rightLowerLeg2Id][1]= event.srcElement.value;
+    initNodes(rightLowerLeg2Id);
+}
+
+function moveRightLower2Z(){
+    alpha[rightLowerLeg2Id][2]= event.srcElement.value;
+    initNodes(rightLowerLeg2Id);
+}
+
+//THIRD right LEG
+function moveRightUpper3X(){
+    alpha[rightUpperLeg3Id][0]= event.srcElement.value;
+    initNodes(rightUpperLeg3Id);
+}
+
+function moveRightUpper3Y(){
+    alpha[rightUpperLeg3Id][1]= event.srcElement.value;
+    initNodes(rightUpperLeg3Id);
+}
+
+function moveRightUpper3Z(){
+    alpha[rightUpperLeg3Id][2]= event.srcElement.value;
+    initNodes(rightUpperLeg3Id);
+}
+
+function moveRightMid3X(){
+    alpha[rightMidLeg3Id][0]= event.srcElement.value;
+    initNodes(rightMidLeg3Id);
+}
+
+function moveRightMid3Y(){
+    alpha[rightMidLeg3Id][1]= event.srcElement.value;
+    initNodes(rightMidLeg3Id);
+}
+
+function moveRightMid3Z(){
+    alpha[rightMidLeg3Id][2]= event.srcElement.value;
+    initNodes(rightMidLeg3Id);
+}
+
+function moverightLower3X(){
+    alpha[rightLowerLeg3Id][0]= event.srcElement.value;
+    initNodes(rightLowerLeg3Id);
+}
+
+function moveRightLower3Y(){
+    alpha[rightLowerLeg3Id][1]= event.srcElement.value;
+    initNodes(rightLowerLeg3Id);
+}
+
+function moveRightLower3Z(){
+    alpha[rightLowerLeg3Id][2]= event.srcElement.value;
+    initNodes(rightLowerLeg3Id);
+}
+
+//FOURTH right LEG
+function moveRightUpper4X(){
+    alpha[rightUpperLeg4Id][0]= event.srcElement.value;
+    initNodes(rightUpperLeg4Id);
+}
+
+function moveRightUpper4Y(){
+    alpha[rightUpperLeg4Id][1]= event.srcElement.value;
+    initNodes(rightUpperLeg4Id);
+}
+
+function moveRightUpper4Z(){
+    alpha[rightUpperLeg4Id][2]= event.srcElement.value;
+    initNodes(rightUpperLeg4Id);
+}
+
+function moveRightMid4X(){
+    alpha[rightMidLeg4Id][0]= event.srcElement.value;
+    initNodes(rightMidLeg4Id);
+}
+
+function moveRightMid4Y(){
+    alpha[rightMidLeg4Id][1]= event.srcElement.value;
+    initNodes(rightMidLeg4Id);
+}
+
+function moveRightMid4Z(){
+    alpha[rightMidLeg4Id][2]= event.srcElement.value;
+    initNodes(rightMidLeg4Id);
+}
+
+function moveRightLower4X(){
+    alpha[rightLowerLeg4Id][0]= event.srcElement.value;
+    initNodes(rightLowerLeg4Id);
+}
+
+function moveRightLower4Y(){
+    alpha[rightLowerLeg4Id][1]= event.srcElement.value;
+    initNodes(rightLowerLeg4Id);
+}
+
+function moveRightLower4Z(){
+    alpha[rightLowerLeg4Id][2]= event.srcElement.value;
+    initNodes(rightLowerLeg4Id);
+}
